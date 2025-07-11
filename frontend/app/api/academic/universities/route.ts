a148fe86398f2368d8d51ac6d58894e36b445eab
MohamedHassan40 <mohamedelorbany10@gmail.com>
2025-07-11 10:37:10 +0300

    l

--- frontend/app/api/academic/universities/route.ts
+++ frontend/app/api/academic/universities/route.ts
@@ -1,6 +1,6 @@
-export async function GET() {
-  const res = await fetch(`${process.env.BACKEND_URL}api/academic/universities/`);
-  const data = await res.json();
-  let universities = Array.isArray(data) ? data : data.results || [];
-  return Response.json(universities);
-} 
+// export async function GET() {
+//   const res = await fetch(`${process.env.BACKEND_URL}api/academic/universities/`);
+//   const data = await res.json();
+//   let universities = Array.isArray(data) ? data : data.results || [];
+//   return Response.json(universities);
+// } 
